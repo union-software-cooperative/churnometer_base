5ceaf4f8e7b729b6bfd4e4e00cd033e8aee396d0
Mat Holroyd <matholroyd@gmail.com>
2012-02-02 12:43:17 +1100

    Got fancy index page working

--- start.rb
+++ start.rb
@@ -2,12 +2,48 @@ require 'rubygems'
 require 'sinatra'
 require 'erb'
 require 'pg'
+require 'sass'
 
-get '/summary' do
-  @data = db.ex("select * from churnsummarydyn7('org', '2011-10-6', '2012-1-3',true, '<search><branchid>NG</branchid><status>1</status><status>14</status></search>')")
+get '/' do
+  erb :index  
+end
+
+post '/get_data' do
+  @data = db.ex params[:sql]
   erb :summary
 end
 
+get '/get_data' do
+  @data = db.ex params[:sql]
+  erb :summary
+end
+
+get '/stylesheets/:name.css' do |name|
+  scss name.to_sym, :style => :expanded
+end
+
+
+get '/summary' do
+  @data = db.ex summary_sql
+  erb :summary
+end
+
+def summary_sql
+  xml = "<search><branchid>NG</branchid><status>1</status><status>14</status></search>"
+  
+  # org branchid companyid status lead area del hsr
+  # nuwelectorate state feegroup
+  
+  <<-SQL 
+    select * 
+    from churnsummarydyn7('org', 
+                          '2011-10-6', 
+                          '2012-1-3',
+                          true, 
+                          '#{xml}'
+                          )
+  SQL
+end
 
 def db
   @db ||= Db.new
