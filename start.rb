3debfdb3f31272fcff316be3cf5f1f872ff1199c
Mat Holroyd <matholroyd@gmail.com>
2012-02-02 17:23:35 +1100

    Fixed merging of filters with defaults, so does deep merging

--- start.rb
+++ start.rb
@@ -82,7 +82,9 @@ def defaults
     'filter' => {
       'status' => [1, 14]      
     }
-  }.merge(params)
+  }.rmerge(params)
+  
+  
 end
 
 # select displaytext from displaytext where attribute = 'org' and id='dpegg'
@@ -139,4 +141,24 @@ class Db
     @conn.exec(sql)
   end
   
+end
+
+module HashRecursiveMerge
+  def rmerge!(other_hash)
+    merge!(other_hash) do |key, oldval, newval| 
+        oldval.class == self.class ? oldval.rmerge!(newval) : newval
+    end
+  end
+
+  def rmerge(other_hash)
+    r = {}
+    merge(other_hash)  do |key, oldval, newval| 
+      r[key] = oldval.class == self.class ? oldval.rmerge(newval) : newval
+    end
+  end
+end
+
+
+class Hash
+  include HashRecursiveMerge
 end
