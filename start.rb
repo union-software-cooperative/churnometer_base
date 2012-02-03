03e8f9f02b1b52a86a00b2a331c88fda8b397556
Mat Holroyd <matholroyd@gmail.com>
2012-02-03 14:31:13 +1100

    Added simple authentication

--- start.rb
+++ start.rb
@@ -24,6 +24,10 @@ FilterNames = "fn"
 
 include Churnobyl::DataSql
 
+use Rack::Auth::Basic do |username, password|
+  username == 'admin' && password == 'adminpass'
+end
+
 get '/' do
   erb :index  
 end
