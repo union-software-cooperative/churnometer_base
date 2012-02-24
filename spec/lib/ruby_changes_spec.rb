a530a8ec19a07f7c83c1a3438de3fc222b5ed1b4
Mat Holroyd <matholroyd@gmail.com>
2012-02-24 15:00:10 -0800

    Included all lib files in spec_helper

--- spec/lib/ruby_changes_spec.rb
+++ spec/lib/ruby_changes_spec.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../../spec_helper.rb', __FILE__)
-require File.expand_path('../../../lib/ruby_changes.rb', __FILE__)
 
 describe "Ruby changes" do
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,5 @@
+Dir[("./lib/*.rb")].each {|f| require f}
+
+
 # Uncomment if add spec support files
 # Dir[("./support/**/*.rb")].each {|f| require f}
