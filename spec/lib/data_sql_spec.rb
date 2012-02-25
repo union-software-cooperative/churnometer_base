13993c405c35bb49258ae87c548038fcbc3ceacf
Mat Holroyd <matholroyd@gmail.com>
2012-02-24 16:14:37 -0800

    Simple hardcoded membersql spec

--- spec/lib/data_sql_spec.rb
+++ spec/lib/data_sql_spec.rb
@@ -4,9 +4,14 @@ describe Churnobyl::DataSql do
   let(:datasql) do
     class Dummy
       include Churnobyl::DataSql
+      include Churnobyl::Helpers
 
       def initialize
         self.params = {}
+      end
+      
+      def leader?
+        true
       end
 
       attr_accessor :params
@@ -28,4 +33,19 @@ describe Churnobyl::DataSql do
       }
     end
   end
+  
+  describe 'member_sql' do
+    it 'real-world data' do
+      datasql.params.rmerge!({
+        'startDate' => '2012-02-01',
+        'endDate'   => '2012-02-01'
+      })
+      compress(datasql.member_sql).should == "select * from churndetailfriendly20( 'memberfacthelperpaying2', 'branchid', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
+    end
+  end
+  
+  def compress(s)
+    s.gsub(/\s+/, ' ').gsub(/^\s|\s$/, '')
+  end
+  
 end
