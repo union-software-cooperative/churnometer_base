ab1d5d3848e37f7225088bed91b883fd2929b960
Mat Holroyd <matholroyd@gmail.com>
2012-02-24 16:37:41 -0800

    Refactored specs + added some more

--- spec/lib/data_sql_spec.rb
+++ spec/lib/data_sql_spec.rb
@@ -35,16 +35,40 @@ describe Churnobyl::DataSql do
   end
   
   describe 'raw sql checks' do 
-    describe 'member_sql' do
+    before :each do
+      datasql.params.rmerge!({
+        'startDate' => '2012-02-01',
+        'endDate'   => '2012-02-01'
+      })
+    end
+    
+    describe 'summary_sql' do
       it do
-        datasql.params.rmerge!({
-          'startDate' => '2012-02-01',
-          'endDate'   => '2012-02-01'
-        })
-        compress(datasql.member_sql).should == "select * from churndetailfriendly20( 'memberfacthelperpaying2', 'branchid', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
+        compress(datasql.summary_sql).should == "select * from churnsummarydyn19( 'memberfacthelperpaying2', 'branchid', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
+      end
+
+      it do
+        datasql.params.rmerge!({'group_by' => 'lead'})
+        compress(datasql.summary_sql).should == "select * from churnsummarydyn19( 'memberfacthelperpaying2', 'lead', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
       end
     end
+    
+    describe 'member_sql' do
+      it do
+        compress(datasql.member_sql).should == "select * from churndetailfriendly20( 'memberfacthelperpaying2', 'branchid', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
+      end
 
+      it do
+        datasql.params.rmerge!({'group_by' => 'lead'})
+        compress(datasql.member_sql).should == "select * from churndetailfriendly20( 'memberfacthelperpaying2', 'lead', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
+      end
+
+      it do
+        datasql.params.rmerge!({'group_by' => 'areaid'})
+        compress(datasql.member_sql).should == "select * from churndetailfriendly20( 'memberfacthelperpaying2', 'areaid', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
+      end
+    end
+    
     def compress(s)
       s.gsub(/\s+/, ' ').gsub(/^\s|\s$/, '')
     end
