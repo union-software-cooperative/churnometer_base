a87f120f065d4ab23f95c45e28141c309d5fb23f
Mat Holroyd <matholroyd@gmail.com>
2012-02-24 16:15:54 -0800

    Refactoring spec a bit, so can grow easily

--- spec/lib/data_sql_spec.rb
+++ spec/lib/data_sql_spec.rb
@@ -34,18 +34,21 @@ describe Churnobyl::DataSql do
     end
   end
   
-  describe 'member_sql' do
-    it 'real-world data' do
-      datasql.params.rmerge!({
-        'startDate' => '2012-02-01',
-        'endDate'   => '2012-02-01'
-      })
-      compress(datasql.member_sql).should == "select * from churndetailfriendly20( 'memberfacthelperpaying2', 'branchid', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
+  describe 'raw sql checks' do 
+    describe 'member_sql' do
+      it do
+        datasql.params.rmerge!({
+          'startDate' => '2012-02-01',
+          'endDate'   => '2012-02-01'
+        })
+        compress(datasql.member_sql).should == "select * from churndetailfriendly20( 'memberfacthelperpaying2', 'branchid', '', '2012-02-01', '2012-02-02', true, '<search><status>1</status><status>14</status></search>' )"
+      end
+    end
+
+    def compress(s)
+      s.gsub(/\s+/, ' ').gsub(/^\s|\s$/, '')
     end
   end
   
-  def compress(s)
-    s.gsub(/\s+/, ' ').gsub(/^\s|\s$/, '')
-  end
   
 end
