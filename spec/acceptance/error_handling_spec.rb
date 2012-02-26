86cf8e08ace5be8e5acd6e2c0a9d98593cec7b66
Mat Holroyd <matholroyd@gmail.com>
2012-02-26 13:21:00 -0800

    Added simple specs for error handling

---
+++ spec/acceptance/error_handling_spec.rb
@@ -0,0 +1,31 @@
+require File.expand_path(File.dirname(__FILE__) + "/acceptance_helper")
+
+class Churnobyl
+  # Override authentication includes
+  def leader?
+    true
+  end
+  
+  def protected!
+  end
+end
+
+describe "Not found" do
+  it "shows appropriate error" do
+    visit "/something-that-does-not-exist"
+   
+    page.should have_content "Route does not exist"
+  end
+end
+
+describe "Internal error" do
+  it "shows stack trace" do
+    visit "/?startDate=20-01-01"
+    
+    page.should have_content "date/time field value out of range"
+    page.should have_content "/?startDate=20-01-01"
+    page.should have_content '{"startDate"=>"20-01-01", "splat"=>[], "captures"=>[#]}'
+    page.should have_content "select * from churnsummarydyn19( 'memberfacthelperpaying2', 'branchid', '', '20-01-01', '2012-02-27', true, '114' )"
+    page.select have_content "/churnobyl/lib/db.rb"
+  end
+end
