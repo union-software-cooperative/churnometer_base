27e8b1b502b6e09671c9456be643e138d7916a5f
David Beswick <dlbeswick@gmail.com>
2012-08-21 16:20:39 +1000

    Modified request to more closely match database structure.

--- lib/services/autocomplete_displaytext.rb
+++ lib/services/autocomplete_displaytext.rb
@@ -2,6 +2,18 @@ require './lib/services/autocomplete'
 
 # Classes of autocomplete services that query the "displaytext" table
 class ServiceAutocompleteDisplaytext < ServiceAutocomplete
+  def initialize(db, param_hash)
+    super
+
+    attribute = param_hash['attribute']
+    search = param_hash['search']
+
+    raise "No attribute supplied." if attribute.nil?
+    raise "No search parameter supplied." if search.nil?
+
+    @query = sql_text(db, attribute, search)
+  end
+
 protected
   def json_to_db_column_mapping
     {
@@ -11,8 +23,8 @@ protected
     }
   end
 
-  def sql_text(search_string)
-    "select id, displaytext from displaytext where attribute = \'#{sql_attribute()}\' and displaytext ilike \'%#{search_string.gsub('\'','\'\'')}%\' order by displaytext"
+  def sql_text(db, attribute, search_string)
+    "select id, displaytext from displaytext where attribute = #{db.db.quote(attribute)} and displaytext ilike #{db.db.quote('%'+search_string+'%')} order by displaytext"
   end
 end
 
