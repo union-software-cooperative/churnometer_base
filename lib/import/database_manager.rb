329bfc2b2ee39e828be8cc4d7cfcb985be90a7db
Luke Rohde <lukerohde@gmail.com>
2012-10-08 15:19:09 +1100

    db generation and importing

---
+++ lib/import/database_manager.rb
@@ -0,0 +1,400 @@
+require './lib/churn_db'
+require 'open3.rb'
+
+class DatabaseManager
+  
+  def db 
+    @db ||= Db.new
+  end
+
+  def dimensions
+    @dimensions ||= Dimensions.new # Stub of David's dimension class
+  end
+
+  def migratecols_sql()
+    sql = <<-SQL
+      drop function if exists insertmemberfact();
+      drop view if exists memberchangefromlastchange;
+      drop view if exists lastchange;
+      drop view if exists memberfacthelperquery;
+
+      drop table if exists membersourceprev_migration;
+      drop table if exists memberfact_migration;
+      
+	    alter table membersourceprev rename to membersourceprev_migration;
+      alter table memberfact rename to memberfact_migration;
+     
+      drop table if exists membersource;
+      
+      #{rebuild_displaytextsource_sql};
+      #{rebuild_transactionsource_sql};
+      #{rebuild_membersource_sql};
+      #{rebuild_membersourceprev_sql};
+      #{memberfact_sql};
+      #{lastchange_sql};
+      #{memberchangefromlastchange_sql};
+      #{memberfacthelperquery_sql};
+      #{insertmemberfact_sql};
+    SQL
+  end
+  
+  def migratecols()
+    db.ex(migratecols_sql)
+  end
+  
+  def rebuild_displaytextsource_sql
+    sql = <<-SQL
+      drop table if exists displaytextsource;
+      
+      create table displaytextsource
+      (
+        attribute varchar(255) not null
+        , id varchar(255) null
+        , displaytext varchar(255) null
+      );
+    SQL
+  end
+  
+  def rebuild_displaytextsource
+    db.ex(rebuild_displaytextsource_sql)
+  end
+  
+  def rebuild_transactionsource_sql
+    sql = <<-SQL
+      drop table if exists transactionsource;
+      
+      create table transactionsource
+      (
+        id varchar(255) not null
+        , creationdate timestamp not null
+        , memberid varchar(255) not null
+        , userid varchar(255) not null
+        , amount numeric not null
+      );
+    SQL
+  end
+  
+  def rebuild_transactionsource
+    db.ex(rebuild_transactionsource_sql)
+  end
+  
+   def membersource_sql
+    sql = <<-SQL
+      create table membersource 
+      (
+        memberid varchar(255) not null
+        , status varchar(255) not null
+    SQL
+  
+    dimensions.each { |d| sql << <<-REPEAT } 
+        , #{d.column_base_name} varchar(255) null
+    REPEAT
+    
+    sql << <<-SQL
+      )
+    SQL
+  end
+  
+  def rebuild_membersource_sql
+    sql = <<-SQL
+      drop table if exists membersource;
+    
+      #{membersource_sql}
+    SQL
+  end
+  
+  def empty_membersource
+    db.ex("delete from membersource");
+  end
+
+  def rebuild_membersource
+    db.ex(rebuild_membersource_sql)
+  end
+  
+  def rebuild_membersourceprev_sql
+    rebuild_membersource_sql.sub('membersource', 'membersourceprev')
+  end
+  
+  def rebuild_membersourceprev
+    db.ex(rebuild_membersourceprev_sql)
+  end
+
+  def memberfact
+    sql = <<-SQL
+      create table memberfact
+      (
+        changeid BIGSERIAL PRIMARY KEY
+        , changedate timestamp not null
+        , memberid varchar(255) not null
+        , oldstatus varchar(255) null
+        , newstatus varchar(255) null
+    SQL
+
+    dimensions.each { | i | sql << <<-REPEAT }
+        , old#{d.column_base_name} varchar(255) null
+        , new#{d.column_base_name} varchar(255) null
+    REPEAT
+
+    sql << <<-SQL
+      )
+    SQL
+  end
+
+  def lastchange
+    sql = <<-SQL
+      create view lastchange as
+        select
+          changeid
+          , changedate
+          , memberid
+          , newstatus status
+    SQL
+
+    dimensions.each { |d| sql << <<-REPEAT }
+          , new#{d.column_base_name} as col#{d.column_base_name}
+    REPEAT
+
+    sql << <<-SQL
+        from 
+          memberfact 
+        where (
+          memberfact.changeid IN (
+            select 
+              max(memberfact.changeid) AS max 
+            from  
+              memberfact 
+            group by  
+              memberfact.memberid
+          )
+        );
+    SQL
+  end
+
+  def memberchangefromlastchange
+
+    sql = <<-SQL
+      -- find changes, adds and deletions in latest data
+      create view memberchangefromlastchange as 
+      -- find members who've changed in latest data
+      select
+        now() as changedate
+        , old.memberid
+        , old.status as oldstatus
+        , new.status as newstatus
+    SQL
+    
+    dimensions.each { |d| sql << <<-REPEAT }
+        , old.#{d.column_base_name} as old#{d.column_base_name}
+        , new.#{d.column_base_name} as new#{d.column_base_name}
+    REPEAT
+
+    sql << <<-SQL
+      from
+        lastchange old
+        inner join membersource new on old.memberid = new.memberid
+      where
+        1=0 -- so I don't have to stript the following OR
+    SQL
+
+    dimensions.each { |d| sql << <<-REPEAT }
+        OR coalesce(old.#{d.column_base_name}, '') <> coalesce(new.#{d.column_base_name}) 
+    REPEAT
+    
+    sql << <<-SQL
+      UNION ALL
+      -- find members missing in latest data
+      select
+        now() as changedate
+        , old.memberid
+        , old.status as oldstatus
+        , null as newstatus
+    SQL
+
+    dimensions.each { |d| sql << <<-REPEAT }
+        , old.#{d.column_base_name} as old#{d.column_base_name}
+        , null as new#{d.column_base_name}
+    REPEAT
+
+    sql << <<-SQL
+      from
+        lastchange old
+      where
+        not old.memberid in (
+          select
+            memberid
+          from
+            membersource
+        )
+        AND not (
+          old.status is null
+    SQL
+
+    dimensions.each { |d| sql << <<-REPEAT }
+          AND old.#{d.column_base_name} is null
+    REPEAT
+
+    sql << <<-SQL
+        )
+      UNION ALL
+      -- find members appearing in latest data
+      select
+        now() as changedate
+        , new.memberid
+        , null as oldstatus
+        , new.status as newstatus
+    SQL
+
+    dimensions.each { |d| sql << <<-REPEAT }
+        , null as old#{d.column_base_name}
+        , new.#{d.column_base_name} as new#{d.column_base_name}
+    REPEAT
+
+    sql << <<-SQL
+      from
+        membersource new
+      where
+        not new.memberid in (
+          select
+            lastchange.memberid
+          from
+            lastchange
+        )
+    SQL
+  end
+
+  def insertmemberfact
+
+    sql = <<-SQL
+      CREATE OR REPLACE FUNCTION insertmemberfact() RETURNS void 
+	    AS $BODY$begin
+
+        -- don't run the import if nothing is ready for comparison
+        if 0 = (select count(*) from membersource) then 
+	        return; 
+        end if;     
+
+        insert into memberfact (
+          changedate
+          , memberid
+          , oldstatus
+          , newstatus
+    SQL
+
+    dimensions.each { |d| sql << <<-REPEAT }
+          , old#{d.column_base_name}
+          , new#{d.column_base_name}
+    REPEAT
+
+    sql << <<-SQL
+        )
+        select
+          changedate
+          , memberid
+          , oldstatus
+          , newstatus
+    SQL
+
+    dimensions.each { |d| sql << <<-REPEAT }
+          , old#{d.column_base_name}
+          , new#{d.column_base_name}
+    REPEAT
+
+    sql << <<-SQL
+        from
+          memberchangefromlastchange;
+
+      end$BODY$
+      LANGUAGE plpgsql
+	    COST 100
+	    CALLED ON NULL INPUT
+	    SECURITY INVOKER
+	    VOLATILE;
+    SQL
+  end
+
+  def memberfacthelperquery
+
+    sql = <<-SQL
+      create view memberfacthelperquery as
+        select
+          changeid
+          , changedate
+          , memberid      
+          , -1 as net
+          , 0 as gain
+          , 1 as loss
+          , coalesce(oldstatus, '') as status
+          , coalesce(newstatus, '') as _status
+          , case when coalesce(oldstatus, '') <> coalesce(newstatus, '')
+              then -1 else 0 end as statusdelta
+          , 0 as a1pgain
+          , case when coalesce(oldstatus, '') = 'a1p' and coalesce(newstatus, '') <> 'a1p'
+              then -1 else 0 end as a1ploss
+          , 0 as payinggain
+          , case when coalesce(oldstatus, '') = 'paying' and coalesce(newstatus, '') <> 'paying'
+            then -1 else 0 end as payingloss
+          , 0 as stoppedgain
+          , case when coalesce(oldstatus, '') = 'stopped' and coalesce(newstatus, '') <> 'stopped'
+              then -1 else 0 end as stoppedloss
+          , 0 as othergain
+          , case when 
+              NOT coalesce(oldstatus, '') = 'a1p' and coalesce(newstatus, '') <> 'a1p'
+              AND NOT coalesce(oldstatus, '') = 'paying' and coalesce(newstatus, '') <> 'paying'
+              AND NOT coalesce(oldstatus, '') = 'stopped' and coalesce(newstatus, '') <> 'stopped'
+              then -1 else 0 end as otherloss
+    SQL
+  
+    dimensions.each { |d| sql << <<-REPEAT }
+          , coalesce(old#{d.column_base_name}, '') #{d.column_base_name}
+          , case when coalesce(old#{d.column_base_name}, '') <> coalesce(new#{d.column_base_name}, '')
+              then -1 else 0 end as #{d.column_base_name}delta 
+    REPEAT
+
+    sql << <<-SQL
+        from 
+          memberfact
+
+        UNION ALL
+
+        select
+          changeid
+          , changedate
+          , memberid
+          , 1 as net
+          , 1 as gain
+          , 0 as loss
+          , coalesce(newstatus, '') as status
+          , coalesce(oldstatus, '') as _status
+          , case when coalesce(oldstatus, '') <> coalesce(newstatus, '')
+              then 1 else 0 end as statusdelta
+          , case when coalesce(oldstatus, '') = 'a1p' and coalesce(newstatus, '') <> 'a1p'
+              then 1 else 0 end as a1pgain
+          , 0 as a1ploss
+          , case when coalesce(oldstatus, '') = 'paying' and coalesce(newstatus, '') <> 'paying'
+            then 1 else 0 end as payinggain
+          , 0 as payingloss
+          , case when coalesce(oldstatus, '') = 'stopped' and coalesce(newstatus, '') <> 'stopped'
+              then 1 else 0 end as stoppedgain
+          , 0 as stoppedloss
+          , case when 
+              NOT coalesce(oldstatus, '') = 'a1p' and coalesce(newstatus, '') <> 'a1p'
+              AND NOT coalesce(oldstatus, '') = 'paying' and coalesce(newstatus, '') <> 'paying'
+              AND NOT coalesce(oldstatus, '') = 'stopped' and coalesce(newstatus, '') <> 'stopped'
+              then 1 else 0 end as othergain
+          , 0 as otherloss
+    SQL
+  
+    dimensions.each { |d| sql << <<-REPEAT }
+          , coalesce(new#{d.column_base_name}, '') #{d.column_base_name}
+          , case when coalesce(old#{d.column_base_name}, '') <> coalesce(new#{d.column_base_name}, '')
+              then 1 else 0 end as #{d.column_base_name}delta 
+    REPEAT
+
+    sql << <<-SQL
+      from
+        memberfact
+    SQL
+  end
+  
+  
+end
---
+++ lib/import/import_presenter.rb
@@ -0,0 +1,123 @@
+require './lib/churn_db'
+require 'open3.rb'
+
+class ImportPresenter
+
+  def db 
+    @db ||= Db.new
+  end
+  
+  def dimensions
+    @dimensions ||= Dimensions.new # Dimensions is stubbed out, expecting integration with David's column generalisation code
+  end
+  
+  def dbm
+    @dbm ||= DatabaseManager.new
+  end
+  
+  def reset
+    dbm.empty_membersource
+    dbm.rebuild_displaytextsource
+    dbm.rebuild_transactionsource
+  end
+  
+  def go
+    db.ex("select insertmemberfact()");
+    db.ex("select inserttransactionfact()");
+    db.ex("select updatedisplaytext()");
+  end
+  
+  def status
+		mcnt = membersource_count
+		dcnt = displaytextsource_count
+		tcnt = transactionsource_count
+
+  	mcnt_msg = (mcnt == "0" ? "No member data staged - expecting members.txt to be upload" : mcnt.to_s + " rows of member data staged for import")
+  	dcnt_msg = (dcnt == "0" ? "No displaytext data staged - expecting displaytext.txt to be upload" : dcnt.to_s + " rows of displaytext data staged for import")
+  	tcnt_msg = (tcnt == "0" ? "No transaction data staged - expecting transactions.txt to be upload" : tcnt.to_s + " rows of transaction data staged for import")
+  	
+  	<<-HTML
+  		<ul>
+  			<li>
+  				#{mcnt_msg}
+  			</li>
+  			<li>
+  				#{dcnt_msg}
+  			</li>
+  			<li>
+  				#{tcnt_msg}
+  			</li>
+  		</ul>
+		HTML
+  end
+  
+  def membersource_count
+    data = db.ex("select count(*) cnt from membersource")
+  	data[0]['cnt']
+  end
+  
+  def displaytextsource_count
+    data = db.ex("select count(*) cnt from displaytextsource")
+  	data[0]['cnt']
+  end
+  
+  def transactionsource_count
+    data = db.ex("select count(*) cnt from transactionsource")
+  	data[0]['cnt']
+  end
+  
+  def console_ex(cmd)
+    err=nil
+    result=nil
+    
+    Open3.popen3(cmd) do |i,o,e,t| 
+      i.close_write # don't pipe anything in to stdin
+      err = e.read
+      result = o.read
+    end
+    if !err.nil? && !err.empty? 
+      raise err
+    end
+    result
+  end
+  
+  def member_import(file)
+    dbm.empty_membersource
+    console_ex(member_import_command(file))
+  end
+
+  def displaytext_import(file)
+    dbm.rebuild_displaytextsource
+    console_ex(displaytext_import_command(file))
+  end
+  
+  def transaction_import(file)
+    dbm.rebuild_transactionsource
+    console_ex(transaction_import_command(file))
+  end
+  
+  def member_import_command(file)
+  	cmd = "psql -h localhost churnometer -c \"\\copy membersource (memberid, status" 
+    
+    dimensions.each do |d|
+    	cmd << ", #{d.column_base_name}" 
+    end
+    
+    cmd << ") from '#{file}' with delimiter as E'\\t' null as '' CSV HEADER\""
+  end
+  
+  def displaytext_import_command(file)
+  	cmd = "psql -h localhost churnometer -c \""
+  	cmd << "\\copy displaytextsource (attribute, id, displaytext) " 
+    cmd << "from '#{file}' with delimiter as E'\\t' null as '' CSV HEADER"
+    cmd << "\""
+  end
+  
+  def transaction_import_command(file)
+  	cmd = "psql -h localhost churnometer -c \""
+  	cmd << "\\copy transactionsource (id, creationdate, memberid, userid, amount) " 
+    cmd << "from '#{file}' with delimiter as E'\\t' null as '' CSV HEADER"
+    cmd << "\""
+  end
+  
+end
