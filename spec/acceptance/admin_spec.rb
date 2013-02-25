fab1bb56c1ff6a54c0decc8deb7b1264b82adfbc
David Beswick <dlbeswick@gmail.com>
2013-02-25 15:52:50 +1100

    Tests work with regression database and config. Added email-on-error option accessors to ChurnometerApp, which are verified on startup. Removed some tests that are only applicable to NUW churnometer.

--- spec/acceptance/admin_spec.rb
+++ spec/acceptance/admin_spec.rb
@@ -81,8 +81,8 @@ describe "admin" do
   
   it "has a data entry group by option that works" do
     visit "/"
-    
-    page.should have_select('group_by', :options => ['Data Entry'])
+
+    page.should have_select('group_by', :with_options => ['Data Entry'])
     
     select "Data Entry", :from => "group_by"
     click_button "Refresh"
@@ -93,49 +93,40 @@ describe "admin" do
     page.should have_content("Running total") 
     page.should have_no_content "Error"
 
-    test_app().roles.get_mandatory($testing_role).summary_data_tables.each do |table|
-      click_link(table.display_name)
+    page.should have_selector(:link, :text=>"Summary")
+    page.find(:link, :text=>"Summary").click
 
-      page.should have_css("table#table-#{table.display_name.downcase} thead")
+    page.should have_css("table#table-summary thead")
 
-      within "table#table-#{table.display_name.downcase} thead" do
-        tables = test_app().roles.get_mandatory($testing_role).summary_data_tables
-        table_headers = table.column_names.collect do |col_name|
-          test_app().col_names[col_name]
-        end
-
-        # tbd: infer first entry from 'group by'
-        table_header_has ['data entry'] + table_headers.compact
-      end
+    within "table#table-summary thead" do
+      table_header_has
+      ['data entry',
+       'new applications (total)',
+      'applications exited w/out payment',
+      'paying at start date',
+      'started paying',
+      'ceased paying',
+      'paying net',
+      'paying at end date',
+      'left stopped paying cycle (exited)',
+      'unique contributors',
+      'income net']
     end
-    
-    drill_down_into_data_entry
-  end
-  
-  def drill_down_into_data_entry
-    within 'table#table-summary' do
-      click_link "Reception"
-    end
-    
-    page.should have_content("Start Date") 
-    page.should have_content("End Date") 
-    page.should have_content("Group by") 
-    page.should have_content("Running total") 
-    page.should have_no_content "Error"
-    
-    page.should have_content "Data Entry: Reception"
   end
   
   it "has a stopped paying table" do
     visit "/"
     
+    select "Branch", :from => "group_by"
+    click_button "Refresh"
+    
     click_link("Stopped")
     within 'table#table-stopped thead' do
       
-      table_header_has "branch", "stopped paying at start date", "became stopped paying", "became stopped paying not followed up", "stopped paying followed up", "stopped paying resumed paying", "stopped paying transfers in", "stopped paying transfers out", "stopped paying at end date", "became rule59 not followed up"
+      table_header_has "branch", "stopped paying at start date", "entered stopped paying cycle", "entered stopped paying cycle (pending resolution)", "left stopped paying cycle (exited)", "left stopped paying cycle (resumed paying)", "stopped paying transfers in", "stopped paying transfers out", "stopped paying at end date"
     end
     within 'table#table-stopped tfoot' do
-      row_has "", %w{3335 196 179 -317 -20 0 0 3194 89}
+      row_has "", %w{24 	4 	1 	-1 	0 	0 	0 	27}
     end
   end
   
@@ -144,25 +135,11 @@ describe "admin" do
 
       click_link("Stopped")
       within('table#table-stopped tbody tr:nth-child(1)') do
-        click_link "94"
+        click_link "6"
       end
       within('table#table-membersummary tbody tr:nth-child(1)') do 
-        row_has "General Branch", "20 April 2012", "Taiwa, Lionel (NG053785)"
+        row_has "Construction", "19 March 2012", "Fanene, Edan (m45268)"
       end
-      
-      click_link("Follow up")
-      within 'table#table-followup thead' do
-        table_header_has "branch", "changedate", "memberid", "member", "current status", "follow up notes", "payment type", "current contact detail", "current site", "current employer", "payroll/hr contact", "current payment status", "new organiser"
-      end
-  end
-  
-  it "shows employer payment status" do
-    visit "/?column=&startDate=14+August+2011&endDate=26+April+2012&group_by=employerid&interval=none&f%5Bbranchid%5D=NG&f%5Blead%5D=afalconer&f%5Borg%5D=borders&lock%5Bcompanyid%5D="
-    
-    click_link("Remittance")
-    within 'table#table-remittance thead' do
-      table_header_has "employer", "a1p at end date", "paying at end date", "payment type", "current paid to date", "current payment status"
-    end
   end
   
   def row_has(*items)
@@ -176,8 +153,6 @@ describe "admin" do
   def table_header_has(*items)
     items.flatten.each_with_index do |item, i|
       within "th:nth-child(#{i + 1})" do |element|
-        $stderr.puts element.text
-        $stderr.puts "*" + item
         page.should have_content item
       end
     end
