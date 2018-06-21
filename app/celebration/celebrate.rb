2d2d47fe0e79206352b38cf64b7eda90c78e5fdf
LUKE ROHDE <lukerohde@gmail.com>
2018-06-22 09:24:01 +1000

    improve celebration logic to allow for premature completion of celebrations by new celebrations.

--- app/celebration/celebrate.rb
+++ app/celebration/celebrate.rb
@@ -1,23 +1,41 @@
 require 'pg'
 require 'yaml'
 require 'date'
+require 'pry-byebug'
 
 config = YAML.load_file('celebration.yaml')
 db_config = YAML.load_file('../config/config_site.yaml')['database']
 db = PG.connect(db_config.reject{|k,v| k == 'facttable'})
 
-config.each do |c|
-  puts c 
-  f = "#{c['name']}.done"
-  unless File.exist?(f)
-    if db.exec(c['sql']).first['target_met']
+config.each do |k,c|
+  puts "evaluating #{k}..."
+  fd = "#{k}.done"
+  fc = "#{k}.celebrating"
+  unless File.exist?(fd) || File.exists?(fc)
+    result = db.exec(c['sql']).first
+    if result['target_met']
+      puts "celebrating #{k} since #{result['progress']} of #{result['target']} has been achieved."
       `cp #{c['image']} ../public/images/celebration.gif`
       `echo '#{c['caption']}' > ../public/celebration.txt`
-      `touch "#{f}"`
+      Dir['*.celebrating'].each do |f|
+        puts "prematurely completing #{f} due to new celebrations!"
+        `mv #{f} $(basename #{f} .celebrating).done`
+      end
+      `touch "#{fc}"`
+    else 
+      puts "skipping #{k} since only #{result['progress']} of #{result['target']} hasn't been met."
     end
   else
-    if File.mtime(f).to_date + c['duration_days'].to_i <= Date.today  
-      `rm ../public/images/celebration.gif ../public/celebration.txt`
+    if File.exists?(fc)
+      if File.mtime(fc).to_date + c['duration_days'].to_i <= Date.today  
+        puts "completing #{k} celebrations"
+        `rm ../public/images/celebration.gif ../public/celebration.txt`
+        `mv "#{fc}" "#{fd}"`
+      else 
+        puts "still celebrating #{k}"
+      end
+    else 
+      puts "skipping #{k} since it has finished."
     end  
   end
 end 
