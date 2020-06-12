daa07a561ebe4b9ee44cd76fde824d81f3b38f0b
Kim Buckley <kim.g.buckley@gmail.com>
2020-06-12 18:10:35 +1000

    handle remote (non-TTY) execution of backup script

--- scripts/db/backup.sh
+++ scripts/db/backup.sh
@@ -3,5 +3,5 @@ BKP_NAME=${1:-$(date -I)}
 
 # Set working dir relative to script's location
 cd "${0%/*}/../.."
-docker-compose exec db su postgres -c "pg_dump -d churnometer" > app/backup/$(date -I).sql
+docker-compose exec -T db su postgres -c "pg_dump -d churnometer" > app/backup/$(date -I).sql
 gzip app/backup/$(date -I).sql
