61c6c7bb7a485b38d1cda61b4149cec06c9c141c
Kim Buckley <kim.buckley@unitedworkers.org.au>
2024-03-28 12:45:41 +1100

    patch analytics extraction to fetch .gz file

--- scripts/analysis/remote_clone.sh
+++ scripts/analysis/remote_clone.sh
@@ -5,4 +5,5 @@ BKP_NAME=${3:-$(date -I)_logs}
 
 cd "${0%/*}/../.."
 ssh $GT_HOST $GT_PATH/scripts/analysis/write_logs.sh $BKP_NAME
-rsync -Phav "${GT_HOST}:${GT_PATH}/app/backup/${BKP_NAME}.txt" ./app/backup/
+rsync -Phav "${GT_HOST}:${GT_PATH}/app/backup/${BKP_NAME}.csv.gz" ./app/backup/
+gunzip ./app/backup/$BKP_NAME.csv.gz
